29d7609c573927d0ef56202bbc32675a92ed2ff3
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-16 17:44:57 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO users (
+ name,
+ age
+)
+
+VALUES ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ); 
+
+
+INSERT INTO projects (
+ title,
+ category,
+ funding_goal
+)
+
+VALUES ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552); 
+
+
+INSERT INTO pledges (
+ name,
+ amount
+)
+
+VALUES ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564), ("moco", 4564);
+
